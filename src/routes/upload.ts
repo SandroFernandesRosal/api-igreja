cddb90cb1e471e68850996d63452977a784c54ff
Sandro Fernandes <68675374+SandroFernandesRosal@users.noreply.github.com>
2024-02-25 22:52:55 -0300

    armazenamente nuvem google

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,17 +1,27 @@
+import { google } from 'googleapis'
 import { randomUUID } from 'node:crypto'
-import { extname, resolve } from 'node:path'
+import { extname } from 'node:path'
 import { FastifyInstance } from 'fastify'
-import { createWriteStream } from 'node:fs'
-import { pipeline } from 'node:stream'
-import { promisify } from 'node:util'
-
-const pump = promisify(pipeline)
+import { GoogleAuth, OAuth2Client } from 'google-auth-library' // Importando OAuth2Client
+const GOOGLE_API_FOLDER_ID = '1G2Bg5Qs3t5jeB6DM0eZqMUVcwmSaGNzD'
 
 export async function uploadRoutes(app: FastifyInstance) {
+  // Crie uma instância de GoogleAuth
+  const auth = new GoogleAuth({
+    keyFile: './credentials.json', // Certifique-se de que este caminho está correto
+    scopes: ['https://www.googleapis.com/auth/drive'],
+  })
+
+  // Crie um cliente autenticado
+  const client = await auth.getClient()
+
+  // Garanta que o cliente seja do tipo OAuth2Client
+  const oauth2Client = client as OAuth2Client
+
   app.post('/upload', async (request, reply) => {
     const upload = await request.file({
       limits: {
-        fileSize: 5_242_880, // 5mb
+        fileSize: 5_242_880, //   5mb
       },
     })
 
@@ -28,17 +38,28 @@ export async function uploadRoutes(app: FastifyInstance) {
 
     const fileId = randomUUID()
     const extension = extname(upload.filename)
-
     const fileName = fileId.concat(extension)
 
-    const writeStream = createWriteStream(
-      resolve(__dirname, '../../uploads/', fileName),
-    )
+    // Crie um objeto Drive
+    const drive = google.drive({ version: 'v3', auth: oauth2Client })
 
-    await pump(upload.file, writeStream)
+    // Crie um arquivo no Google Drive
+    const fileMetadata = {
+      name: fileName,
+      parents: [GOOGLE_API_FOLDER_ID],
+    }
+    const media = {
+      mimeType: upload.mimetype,
+      body: upload.file,
+    }
+    const response = await drive.files.create({
+      requestBody: fileMetadata,
+      media,
+      fields: 'id',
+    })
 
-    const fullUrl = request.protocol.concat('://').concat(request.hostname)
-    const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
+    // Obtenha a URL do arquivo
+    const fileUrl = `https://drive.google.com/file/d/${response.data.id}/view?usp=sharing`
 
     return { fileUrl }
   })
