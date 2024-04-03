feb4956cf3ad4d1fe475998b0c04766a75578266
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-04-03 19:20:01 -0300

    .

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,6 +4,20 @@ import { prisma } from '../lib/prisma'
 import bcrypt from 'bcryptjs'
 import { v4 as uuidv4 } from 'uuid'
 
+const nodemailer = require('nodemailer')
+
+// Configuração do transporte do Nodemailer para o Gmail
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: true,
+  auth: {
+    user: 'apg.adm.viladapenha@gmail.com', // Substitua pelo seu e-mail do Gmail
+    pass: process.env.PASSWORD_GMAIL, // Substitua pela sua senha do Gmail
+  },
+})
+
 export async function authRoutes(app: FastifyInstance) {
   app.get('/register', async (request) => {
     const users = await prisma.user.findMany()
@@ -27,7 +41,7 @@ export async function authRoutes(app: FastifyInstance) {
     return user
   })
 
-  app.post('/login', async (request) => {
+  app.post('/login', async (request, reply) => {
     const userSchema = z.object({
       login: z.string(),
       password: z.string(),
@@ -56,12 +70,20 @@ export async function authRoutes(app: FastifyInstance) {
           name: user.name,
           avatarUrl: user.avatarUrl,
           login: user.login,
+          id: user.id,
         },
         {
           sub: user.id,
           expiresIn: '30d',
         },
       )
+
+      reply.setCookie('tokenigreja', token, {
+        httpOnly: true,
+        sameSite: 'strict',
+        path: '/',
+        maxAge: 30 * 24 * 60 * 60, // 30 dias
+      })
 
       return { user, token, refreshToken }
     } catch (error) {
@@ -117,6 +139,131 @@ export async function authRoutes(app: FastifyInstance) {
     )
 
     return { token: newToken }
+  })
+
+  app.post('/recover-password-adm', async (request, reply) => {
+    const userSchema = z.object({
+      login: z.string().email({ message: 'Email inválido' }),
+    })
+    const { login } = userSchema.parse(request.body)
+
+    // Verifique se o e-mail existe no banco de dados
+    const user = await prisma.user.findUnique({
+      where: { login },
+    })
+
+    if (!user) {
+      return { error: 'E-mail não encontrado' }
+    }
+
+    // Gere um token de recuperação de senha
+    const token = uuidv4()
+
+    // Armazene o token no banco de dados (exemplo simplificado)
+    await prisma.user.update({
+      data: {
+        passwordResetToken: token,
+        expires: new Date(Date.now() + 3600000), // Expira em 1 hora
+      },
+      where: { login },
+    })
+
+    const mailOptions = {
+      from: 'apg.adm.viladapenha@gmail.com',
+      to: login,
+      subject: 'Recuperação de Senha',
+      text: `Para redefinir sua senha, clique no link abaixo:
+      https://alcancadospelagraca.vercel.app/reset-password/${token}
+       Este link expira em 1 hora.`,
+    }
+
+    transporter.sendMail(mailOptions, (err: any, info: any) => {
+      if (err) {
+        console.error('Erro ao enviar e-mail: ', err)
+        reply
+          .code(500)
+          .send({ error: 'Erro ao enviar e-mail de recuperação de senha' })
+      } else {
+        console.log('E-mail enviado: ' + info.response)
+        reply.send({ message: 'E-mail de recuperação de senha enviado' })
+      }
+    })
+  })
+
+  app.post('/reset-password-adm', async (request, reply) => {
+    try {
+      const resetPasswordSchema = z.object({
+        passwordResetToken: z.string(),
+        login: z.string(),
+        password: z
+          .string()
+          .min(8, { message: 'A senha deve ter pelo menos 8 caracteres' })
+          .max(10, { message: 'A senha deve ter no máximo 10 caracteres' })
+          .regex(/[!@#$%^&*(),.?":{}|<>]/, {
+            message: 'A senha deve conter pelo menos um caractere especial',
+          })
+          .refine((value) => /[a-zA-Z]/.test(value), {
+            message: 'A senha deve conter pelo menos uma letra',
+          }),
+      })
+      const { passwordResetToken, password, login } = resetPasswordSchema.parse(
+        request.body,
+      )
+
+      const user = await prisma.user.findUnique({
+        where: { login },
+      })
+
+      if (!user) {
+        return reply.status(404).send({ error: 'Usuário não encontrado' })
+      }
+
+      if (
+        user.passwordResetToken !== passwordResetToken ||
+        !user.expires ||
+        user.expires < new Date()
+      ) {
+        return reply.status(400).send({ error: 'Token inválido ou expirado' })
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10)
+      await prisma.user.update({
+        where: { login },
+        data: {
+          password: hashedPassword,
+          passwordResetToken,
+        },
+      })
+
+      const token = app.jwt.sign(
+        {
+          id: user.id,
+          name: user.name,
+          avatarUrl: user.avatarUrl,
+          login: user.login,
+        },
+        {
+          sub: user.id,
+          expiresIn: '30d',
+        },
+      )
+
+      reply.setCookie('tokennn', token, {
+        httpOnly: true,
+        sameSite: 'strict',
+        path: '/',
+        maxAge: 30 * 24 * 60 * 60, // 30 dias
+      })
+
+      reply.send({ message: 'Senha redefinida com sucesso' })
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const firstError = error.errors[0]
+        reply.status(400).send({ error: firstError.message })
+      } else {
+        reply.status(500).send({ error: 'Erro interno do servidor' })
+      }
+    }
   })
 
   app.post('/register', async (request) => {
