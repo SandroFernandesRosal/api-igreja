85576e55a79eded7476dd9522632e565e7798329
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-04-13 18:14:11 -0300

    .

--- src/routes/testemunho.ts
+++ src/routes/testemunho.ts
@@ -7,7 +7,7 @@ export async function testemunhoRoutes(app: FastifyInstance) {
     const offsetQuery = (request.query as { offset?: string }).offset
 
     const offset = offsetQuery ? parseInt(offsetQuery, 10) : 0
-    const itemsPerPage = 6
+    const itemsPerPage = 4
 
     const testemunho = await prisma.testemunho.findMany({
       orderBy: {
