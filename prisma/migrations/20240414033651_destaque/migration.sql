a1a654a76fa1118c87940bac768bdab270b334ae
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-04-14 00:41:32 -0300

    .

---
+++ prisma/migrations/20240414033651_destaque/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "New" ADD COLUMN     "destaque" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "NewCaxias" ADD COLUMN     "destaque" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "NewTomazinho" ADD COLUMN     "destaque" BOOLEAN NOT NULL DEFAULT false;
