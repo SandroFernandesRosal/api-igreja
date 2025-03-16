9e9c5dd37c91f3651f9a44fb6fa249e2f802e3ca
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2025-03-16 03:03:33 -0300

    upload em formato webp

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -12,78 +12,86 @@ cloudinary.config({
 
 export async function uploadRoutes(app: FastifyInstance) {
   app.post('/upload', async (request, reply) => {
-    const upload = await request.file({
-      limits: {
-        fileSize: 5_242_880, // 5mb
-      },
-    })
-
-    if (!upload) {
-      return reply.status(400).send()
-    }
-
-    const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/
-    const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
-
-    if (!isValidFileFormat) {
-      return reply.status(400).send()
-    }
-
-    const fileId = randomUUID()
-    const extension = extname(upload.filename)
-    const fileName = fileId.concat(extension)
-
     try {
+      const upload = await request.file({
+        limits: {
+          fileSize: 5_242_880, // 5mb
+        },
+      })
+
+      if (!upload) {
+        console.error('No file uploaded')
+        return reply.status(400).send({ error: 'No file uploaded' })
+      }
+
+      const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/
+      const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
+
+      if (!isValidFileFormat) {
+        console.error('Invalid file format')
+        return reply.status(400).send({ error: 'Invalid file format' })
+      }
+
+      const fileId = randomUUID()
+      const extension = extname(upload.filename)
+      const fileName = fileId.concat(extension)
+
       const fileBuffer = await upload.toBuffer()
       const result = await cloudinary.uploader.upload(
         `data:${upload.mimetype};base64,${fileBuffer.toString('base64')}`,
         {
           resource_type: 'auto',
           public_id: fileName,
+          format: 'webp',
         },
       )
 
       return { fileUrl: result.secure_url }
     } catch (error) {
-      return reply.status(500).send(error)
+      console.error('Error uploading file:', error)
+      return reply.status(500).send({ error: 'Error uploading file' })
     }
   })
 
   app.post('/upload/sobre', async (request, reply) => {
-    const upload = await request.file({
-      limits: {
-        fileSize: 50_000_000, // 50mb
-      },
-    })
-
-    if (!upload) {
-      return reply.status(400).send()
-    }
-
-    const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/
-    const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
-
-    if (!isValidFileFormat) {
-      return reply.status(400).send()
-    }
-
-    const fileId = randomUUID()
-    const extension = extname(upload.filename)
-    const fileName = fileId.concat(extension)
-
     try {
+      const upload = await request.file({
+        limits: {
+          fileSize: 50_000_000, // 50mb
+        },
+      })
+
+      if (!upload) {
+        console.error('No file uploaded')
+        return reply.status(400).send({ error: 'No file uploaded' })
+      }
+
+      const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/
+      const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
+
+      if (!isValidFileFormat) {
+        console.error('Invalid file format')
+        return reply.status(400).send({ error: 'Invalid file format' })
+      }
+
+      const fileId = randomUUID()
+      const extension = extname(upload.filename)
+      const fileName = fileId.concat(extension)
+
       const fileBuffer = await upload.toBuffer()
       const result = await cloudinary.uploader.upload(
         `data:${upload.mimetype};base64,${fileBuffer.toString('base64')}`,
         {
           resource_type: 'auto',
           public_id: fileName,
+          format: 'webp', // Adiciona a transformação para WebP
         },
       )
 
       return { fileUrl: result.secure_url }
     } catch (error) {
-      return reply.status(500).send(error)
+      console.error('Error uploading file:', error)
+      return reply.status(500).send({ error: 'Error uploading file' })
     }
   })
 }
